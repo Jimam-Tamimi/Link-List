1563896d4de77e238d3192989cbbc4ffb2bd99b3
Jimam Tamimi <jimamtamimi12@gmail.com>
2024-10-15 06:09:33 +0600

    added vercel deployment

--- Backend/manage.py
+++ Backend/manage.py
@@ -1,18 +1,22 @@
-# manage.py
-
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django.core.management import execute_from_command_line
-from django.core.wsgi import get_wsgi_application
 
-# Set the default settings module for the 'manage' command.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")  # Adjust if your settings module path is different
 
-# Get the WSGI application for the project.
-application = get_wsgi_application()
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
 
-# Define the handler for Vercel to use.
-handler = application  # This exposes the WSGI application callable as 'handler'
 
-if __name__ == "__main__":
-    execute_from_command_line(sys.argv)  # Run the command line utility
+if __name__ == '__main__':
+    main()
