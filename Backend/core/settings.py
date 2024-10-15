4c6e12358a9ea3983719cf6bcd27719982e774a4
Jimam Tamimi <jimamtamimi12@gmail.com>
2024-10-15 06:49:33 +0600

    db change

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': join('/tmp', 'db.sqlite3'),  # Change this line
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
